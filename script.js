bbb1abcfd26b2ea9cc14dd0c35770cb4281448fa
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-06-28 17:54:00 -0700

    Add the four basic math functions

---
+++ script.js
@@ -0,0 +1,18 @@
+/* The four basic math functions */
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(p, q) {
+    return p * q;
+}
+
+function divide(p, q) {
+    if (q === 0) return null;
+    return p / q;
+}
